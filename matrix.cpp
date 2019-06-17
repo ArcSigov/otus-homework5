d40d052ed4f2b69a35b5c17a447439572394d371
Arcady Sigov <48837966+ArcSigov@users.noreply.github.com>
2019-06-17 12:20:50 +0300

    Add files via upload

--- matrix.cpp
+++ matrix.cpp
@@ -1,45 +1,32 @@
 #include <iostream>
 #include <cassert>
-#include "matrix.h"
+#include "matrix.hpp"
+
 
 int main()
 {
- 	matrix <int, 0> matr;
+ 	 matrix <int, 0> matr;
 
-	matr[0][0] = 0;
-	matr[1][1] = 1;
-	matr[2][2] = 2;
-	matr[3][3] = 3;
-	matr[4][4] = 4;
-	matr[5][5] = 5;
-	matr[6][6] = 6;
-	matr[7][7] = 7;
-	matr[8][8] = 8;
-	matr[9][9] = 9;
-	
-	matr[0][9] = 9;
-	matr[1][8] = 8;
-	matr[2][7] = 7;
-	matr[3][6] = 6;
-	matr[4][5] = 5;
-	matr[5][4] = 4;
-	matr[6][3] = 3;
-	matr[7][2] = 2;
-	matr[8][1] = 1;
-	matr[9][0] = 0;
+	for (auto i = 0 ; i < 10 ; i++)
+	{
+		static auto j = 9;
+		matr[i][i] = i;
+		matr[i][j] = j;
+		j--;
+	}
 
 	for (auto i = 1 ; i < 9 ; i++)
 	{
 		for (auto j = 1 ; j < 9; j++)
 		{
-			std::cout << matr(i,j) << " " ;
+			std::cout << matr[i][j] << " ";
 		}
 		std::cout << std::endl;
 	}
 	
-	std::cout << std::endl;
-	std::cout << "ocuppied cells = " << matr.size()  <<  std::endl;
-	std::cout << std::endl;
+	 std::cout << std::endl;
+	 std::cout << "ocuppied cells = " << matr.size()  <<  std::endl;
+	 std::cout << std::endl;
 
 	for (auto c: matr)
 	{
@@ -50,9 +37,8 @@ int main()
 		std::cout << x << y << v << std::endl;
 	}
 
-	std::cout << std::endl;
-	((matr[100][100] = 314) = 0) = 217;
-	std::cout << matr(100,100) << std::endl;
-	
+	 std::cout << std::endl;
+	 ((matr[100][100] = 314) = 0) = 217;
+	 std::cout << matr[100][100] << std::endl;
 	return 1;
 }
---
+++ matrix.hpp
@@ -0,0 +1,110 @@
+#pragma once
+#include <tuple>
+#include <map>
+#include "matrix_iterator.hpp"
+
+
+
+template<typename T, T def>
+class physical_matrix 
+{
+    static constexpr T _default = def;
+    using matrix_position = std::pair<int,int>;
+public:
+    physical_matrix(){};
+    auto size () const  
+    {
+        return _matr.size();
+    }
+    auto& operator[](const unsigned int pos)
+    {
+        pair.second = pos;
+        return *this;
+    }
+    bool operator == (const T& other)
+    {
+        auto it = _matr.find(pair);
+        if (it!=_matr.end())
+            return it->second == other;
+        return other == def;
+    }
+    auto& operator = (const T& other)
+    {
+        auto it = _matr.find(pair);
+        if (other != _default)
+        {
+            _matr[pair] = other;
+        }   
+        else
+        {
+           if (it != _matr.end())
+           {
+                _matr.erase(it->first);
+           }
+        }
+        return *this;
+    }
+    void set_row(const unsigned int& row)
+    {
+        pair.first = row; 
+    }
+    auto begin() 
+    {
+        return _matr.begin();
+    };  
+    auto end()  
+    {
+        return _matr.end();
+    }
+
+    auto get() const
+    {
+        if (_matr.find(pair) != _matr.end())
+            return _matr.at(pair);
+        return def;
+    }
+
+    friend std::ostream& operator << (std::ostream &out, const physical_matrix<T,def> &phys)
+    {
+        out << phys.get();
+        return out;
+    }
+    
+private:
+    matrix_position pair;
+    std::map<matrix_position,T> _matr{};
+  };
+
+//!< proxy of real matr
+template<typename T, T def>
+class matrix 
+{
+public: 
+    explicit matrix(){};
+    matrix operator = (const matrix& other) = delete;
+    matrix (const matrix& other) = delete;
+
+    auto size () const 
+    {
+        return real.size();
+    }
+
+    auto begin()  
+    {
+        return matrix_iterator<T>(real.begin());
+    }
+
+    auto end()  
+    {
+        return matrix_iterator<T>(real.end());
+    }
+
+    auto& operator [] (const int& row)
+    {
+        real.set_row(row);
+        return real;
+    }
+private:
+     physical_matrix<T,def> real;
+};
+
